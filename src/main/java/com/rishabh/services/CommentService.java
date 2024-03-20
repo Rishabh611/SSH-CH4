c2fb90a3a974296cab14ca3abc01d65622ada04c
Rishabh Singh <rishabsinghrock@gmail.com>
2024-03-20 11:15:45 +0530

    with spring context and DI using qualifier

--- src/main/java/com/rishabh/services/CommentService.java
+++ src/main/java/com/rishabh/services/CommentService.java
@@ -3,6 +3,7 @@ package com.rishabh.services;
 import com.rishabh.model.Comment;
 import com.rishabh.proxies.CommentNotificationProxy;
 import com.rishabh.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +11,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
 
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy){
+    public CommentService(CommentRepository commentRepository,@Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy){
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
